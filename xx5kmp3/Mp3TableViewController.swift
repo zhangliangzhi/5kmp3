38efc81cc46643f915bfd5c97ae29f497e5b5ed7
Zhang LiangZhi <3717591@qq.com>
2017-01-26 01:25:40 +0800

    o

--- xx5kmp3/Mp3TableViewController.swift
+++ xx5kmp3/Mp3TableViewController.swift
@@ -29,23 +29,24 @@ class Mp3TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 14
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
         // Configure the cell...
+        cell.textLabel?.text = "hi i"
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
